5acfe915aac0589805d7c67ea2e7f87e9f3b340f
Gustavo Coelho <gutorc@hotmail.com>
2015-04-27 00:21:18 -0300

    The file to install the packeges necessary to use the application

--- installer.sh
+++ installer.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-packege="default-jre";
 
-if  !(dpkg -s $packege| grep -q  "Status: (install ok installed)");
-then
-	echo "$packege is already installed";
-else
-	apt-get install default-jre;
-fi
-
-
-
+while read packege; do
+	if  dpkg -s "$packege" 2>/dev/null | grep  "Status: install ok installed">/dev/null;
+	then
+		echo "$packege is already installed";
+	else
+		echo "Installing $packege\n";
+		apt-get --yes --force-yes  install "$packege";
+	fi
+done < packeges
