547ac4bdd7ebcc3c8730e707d5ef63d4f8656fd2
pwener <phwener@gmail.com>
2015-07-13 09:17:19 -0300

    Refatorando o script runTransformation.sh
    Signed-off-by: pwener <phwener@gmail.com>
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- transformations/runTransformation.sh
+++ transformations/runTransformation.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-log="/home/pedro/log.file"
-pentahoDirectory="/opt/pentaho/data-integration/kitchen.sh"
+#json="/home/pwener/Documentos/scripts/transformations/tests/104.json"
+json="$1"
+if [ -e $json ]; then
+	echo "Archive json found!"
+else
+	echo "Script fail because it can not find json file"
+	exit 0
+fi
+
+path_json_file=$(cd $(dirname "$json") && pwd -P)/$(basename "$json")
+
+#log="../log.file"
+log="$2"
+if [ -e $log ]; then
+	echo "Log file exist!"
+else
+	touch "$log"
+	echo "Created..."
+fi
+
+path_log_file=$(cd $(dirname "$log") && pwd -P)/$(basename "$log")
+
+
+pentahoDirectory="$HOME/App/data-integration/kitchen.sh"
 job="job.kjb"
-json="../../Downloads/105.json"
-command="$pentahoDirectory -file=\"$job\" -level=RowLevel -param:JSON_FILE=\"$json\" -logfile=\"$log\""
+json="/home/pwener/Documentos/scripts/transformations/tests/104.json"
+command="$pentahoDirectory -file=\"$job\" -level=RowLevel -param:JSON_FILE=\"$path_json_file\" -logfile=\"$path_log_file\""
 echo "$command"
 eval "$command"
