063b05ba5bd96a9fb72538967d03c2391a829ea4
Gustavo Coelho <gutorc@hotmail.com>
2015-07-23 23:12:01 -0300

    Script to create bds necessarily to BiServer
    The script will create the databases need to
    run BiServer using three environment variables.
    These variables have to be set up in the installer
    script. Also, it was created the test for check
    if the script runs finely.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- configureBIServer.sh
+++ configureBIServer.sh
@@ -1,7 +1,9 @@
 #/usr/sh
-
-mysql -u root -p root < ../biserver-ce/data/mysql5/create_jcr_mysql.sql
-mysql -u root -p root < ../biserver-ce/data/mysql5/create_quartz_mysql.sql
-mysql -u root -p root < ../biserver-ce/data/mysql5/create_repository_mysql.sql
+biserverdiretorio=$DIR_BI_SERVER
+host=$HOST_MYSQL
+port=$PORT_MYSQL
+mysql -h $host -P $port -u root -proot < "$biserverdiretorio/biserver-ce/data/mysql5/create_jcr_mysql.sql"
+mysql -h $host -P $port -u root -proot < "$biserverdiretorio/biserver-ce/data/mysql5/create_quartz_mysql.sql"
+mysql -h $host -P $port -u root -proot < "$biserverdiretorio/biserver-ce/data/mysql5/create_repository_mysql.sql"
 
 
