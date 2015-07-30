717314b8acba141cd54392265832bf9415ef86c3
pwener <phwener@gmail.com>
2015-07-30 14:52:22 -0300

    Command to copy all files to config mysql

--- configureBIServer.sh
+++ configureBIServer.sh
@@ -14,4 +14,18 @@ mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_jcr_mys
 mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_quartz_mysql.sql"
 mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_repository_mysql.sql"
 
-echo "Finalized!"
+echo "The Databases required was created!"
+
+echo "Copy files required by biserver with right configuration..."
+
+files_cfg="scripts/filesToConfigureMYSQL/"
+
+#Copy new files, with right configuration
+cp "$files_cfg/context.xml" "/biserver-ce/tomcat/webapps/pentaho/META-INF/"
+cp "$files_cfg/applicationContext-spring-security-hibernate.properties" "/biserver-ce/pentaho-solutions/system/"
+cp "$files_cfg/hibernate-settings.xml" "/biserver-ce/pentaho-solutions/system/hibernate/"
+cp "$files_cfg/mysql5.hibernate.cfg.xml" "/biserver-ce/pentaho-solutions/system/hibernate/"
+cp "$files_cfg/quartz.properties" "/biserver-ce/pentaho-solutions/system/quartz/"
+cp "$files_cfg/repository.xml" "biserver-ce/pentaho-solutions/system/jackrabbit/"
+
+echo "Copy success!"
