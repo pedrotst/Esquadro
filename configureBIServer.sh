92a83f87fd8f6033c88efc84a150ca70336c63cd
pwener <phwener@gmail.com>
2015-07-30 13:45:44 -0300

    Refactor script to configure BIServer

--- configureBIServer.sh
+++ configureBIServer.sh
@@ -1,9 +1,17 @@
 #/usr/sh
-biserverdiretorio=$DIR_BI_SERVER
+
+#BIServer is located in back folder
+cd ../
+
+#Set properties of user
 host=$HOST_MYSQL
 port=$PORT_MYSQL
-mysql -h $host -P $port -u root -proot < "$biserverdiretorio/biserver-ce/data/mysql5/create_jcr_mysql.sql"
-mysql -h $host -P $port -u root -proot < "$biserverdiretorio/biserver-ce/data/mysql5/create_quartz_mysql.sql"
-mysql -h $host -P $port -u root -proot < "$biserverdiretorio/biserver-ce/data/mysql5/create_repository_mysql.sql"
 
+echo "Creating databases of BIserver..."
 
+#Try create this tables
+mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_jcr_mysql.sql"
+mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_quartz_mysql.sql"
+mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_repository_mysql.sql"
+
+echo "Finalized!"
