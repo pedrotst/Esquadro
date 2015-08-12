dd96c07618c3c4e00352d793d500681cf4c3fc49
Gustavo Coelho <gutorc@hotmail.com>
2015-08-12 17:13:43 -0300

    Refactoring the testKeys
    Adding a variable with the path of the temp file.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- testeKeys.sh
+++ testeKeys.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+tmpFile=/tmp/libKeyfile
 while read line; do
-	echo $line>/tmp/testAddKeyfile;
-	key=$(cut -d" " -f1 < /tmp/file);
-	packege=$(cut -d" " -f2 < /tmp/file);
+	echo $line>"$tmpFile";
+	key=$(cut -d" " -f1 < "$tmpFile");
+	packege=$(cut -d" " -f2 < "$tmpFile");
         if  apt-key list | grep  "$key">/dev/null;
         then
                 echo "The $key of the packege: $packege is already added";
@@ -10,7 +11,7 @@ while read line; do
                 echo "In order to install the packege: $packege ,you must run the addKeys.sh";
         fi
 
-	rm /tmp/testAddKeyfile;
+	rm "$tmpFile"
 done < keys
                 
 
