9222f75069e166c91d2ba1a5951df72de8a15490
pwener <phwener@gmail.com>
2015-08-24 16:39:17 -0300

    Parser in beta, working to simple samples

--- Parser.py
+++ Parser.py
@@ -30,6 +30,8 @@ import sys
 METRICS_DIR = "metrics/"
 PROJECT_INFO_FILE = ".esquadro"
 METRICS_CSV = "metrics.csv"
+BLOCK_SEPARATOR = '---'
+NAME_OF_CLASS_INDICATOR = '  - '
 
 # Get root directory
 ROOT = os.environ['HOME']
@@ -58,44 +60,68 @@ def get_info(path_project):
 
 	return contents
 
+#
+def save_metrics(project_metrics, metrics):
+	project_metrics.append(metrics.copy())
+	metrics.clear()
 
 # Converts an csv file of metrics to json
 def metrics_json(path_project):
 	try:
-		metrics_per_file = []
-
-		field_names = ('_filename', '_module')	
+		# Have name of file, package and all metrics about
+		property_by_file = []
+		# It is an type of information inside of csv
+		field_names = ('_filename', '_module')
 
 		with open(ROOT + path_project + METRICS_CSV, 'r') as csv_metrics:
-			lines = csv_metrics.readlines()
+			file_lines = csv_metrics.readlines()
 			file_metrics = {}
-			# Count how many separators have between 
-			separator_count = 0
-			
-			for line_number in range(0, len(lines)):
-				line = lines[line_number]
-				
-				if '---' in line:
-					separator_count += 1
-					if separator_count == 2:
-						metrics_per_file.append(file_metrics.copy())
-						file_metrics.clear()
-						separator_count = 0
 
+			# Too see if an block was initiate
+			# Block is mark by: --- in an csv of analizo
+			block_open = False
+			
+			for line_number in range(0, len(file_lines)):
+				line = file_lines[line_number]
+				
+				if BLOCK_SEPARATOR in line:
+					if block_open:
+						save_metrics(property_by_file, file_metrics)
+					else:
+						block_open = True
+					pass
+				
+				# Yet read!
+				elif NAME_OF_CLASS_INDICATOR in line:
+					pass
+
+				# It is an type of information inside of csv
 				elif field_names[0] in line:
-					try:
-						file_metrics[field_names[0]] = line[line_number + 1]
-					except IndexError:
-						break
+					# Remove /n
+					name_of_class = file_lines[line_number + 1].replace('\n', '')
+					# Remove indicator
+					name_of_class = name_of_class.replace(NAME_OF_CLASS_INDICATOR, '')
+
+					file_metrics[field_names[0]] = name_of_class
+
+				# It is an type of information inside of csv
 				elif field_names[1] in line:
-					file_metrics[field_names[1]] = line.replace('_module: ', '')
+					line = line.replace('_module: ', '').replace('\n', '')
+					file_metrics[field_names[1]] = line
+
+				# Metrics only have name and value, separated by ':'
 				else:
 					metric = line.split(':')
 					file_metrics[metric[0]] = float(metric[1].replace('\n', ''))
+					try:
+						file_lines[line_number + 1]
+					except IndexError:
+						property_by_file.append(file_metrics.copy())
+						file_metrics.clear()
 					
 			csv_metrics.close()
 
-		return metrics_per_file
+		return property_by_file
 
 	except FileNotFoundError:
 		print("File metrics.csv not found!")
@@ -111,15 +137,26 @@ def get_metrics(token):
 	# Get informations about project
 	info_project = get_info(path_project)
 	
-	# Get metric in json
-	metrics = metrics_json(path_project)
-	print(len(metrics))
-
 	# Format to json and write in output
 	info_in_json = json.dumps(info_project)
 
-	output.write(info_in_json)
-	
+	# Write informations opening json declaration with
+	# '[' and adding ',' after this block
+	output.write('[' + info_in_json + ',')
+
+	metrics = metrics_json(path_project)
+
+	# Get all metrics and convert to json
+	for metrics_by_file in metrics:
+		metrics_by_file_json = json.dumps(metrics_by_file)
+		output.write(metrics_by_file_json)
+
+		if(metrics_by_file is not metrics[-1]):
+			output.write(',')
+
+	# Close json
+	output.write(']')
+
 	output.close()
 
 
