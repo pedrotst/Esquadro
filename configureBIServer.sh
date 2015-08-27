79a414ba3dad8c36f18b4763c8ce49a2819bbee5
teste <gutorc@hotmail.com>
2015-08-26 22:07:31 -0300

    Changing the script

--- configureBIServer.sh
+++ configureBIServer.sh
@@ -1,39 +1,41 @@
 #/usr/sh
 
-#BIServer is located in back folder
+if [ "$#" -eq 5 ];
+then
+	workDirectory=$1
+	host_mysql=$2
+	port_mysql=$3
+	user_mysql=$4
+	user_password=$5
+	
+else
+	echo "It is needed 4 arguments" 
+	exit 0;
+fi
 
-#Set properties of user
 
-#Set properties of user
-host=$HOST_MYSQL
-port=$PORT_MYSQL
+echo "Creating databases of BIserver..."
+
 
 echo "Creating databases of BIserver..."
 
 #Try create this tables
-biserverdiretorio=$DIR_BI_SERVER
-host=$HOST_MYSQL
-port=$PORT_MYSQL
-
-echo "Creating databases of BIserver..."
-
-#Try create this tables
-mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_jcr_mysql.sql"
-mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_quartz_mysql.sql"
-mysql -h $host -P $port -u root -proot < "biserver-ce/data/mysql5/create_repository_mysql.sql"
+mysql -h $host_mysql -P $port_mysql -u root -proot < "$workDirectory/biserver-ce/data/mysql5/create_jcr_mysql.sql"
+mysql -h $host_mysql -P $port_mysql -u root -proot < "$workDirectory/biserver-ce/data/mysql5/create_quartz_mysql.sql"
+mysql -h $host_mysql -P $port_mysql -u root -proot < "$workDirectory/biserver-ce/data/mysql5/create_repository_mysql.sql"
 
 echo "The Databases required was created!"
 
 echo "Copy files required by biserver with right configuration..."
 
-files_cfg="scripts/filesToConfigureMYSQL/"
+files_cfg="filesToConfigureMYSQL"
 
 #Copy new files, with right configuration
-cp "$files_cfg/context.xml" "/biserver-ce/tomcat/webapps/pentaho/META-INF/"
-cp "$files_cfg/applicationContext-spring-security-hibernate.properties" "/biserver-ce/pentaho-solutions/system/"
-cp "$files_cfg/hibernate-settings.xml" "/biserver-ce/pentaho-solutions/system/hibernate/"
-cp "$files_cfg/mysql5.hibernate.cfg.xml" "/biserver-ce/pentaho-solutions/system/hibernate/"
-cp "$files_cfg/quartz.properties" "/biserver-ce/pentaho-solutions/system/quartz/"
-cp "$files_cfg/repository.xml" "biserver-ce/pentaho-solutions/system/jackrabbit/"
+cp "$files_cfg/context.xml" "$workDirectory/biserver-ce/tomcat/webapps/pentaho/META-INF/"
+cp "$files_cfg/applicationContext-spring-security-hibernate.properties" "$workDirectory/biserver-ce/pentaho-solutions/system/"
+cp "$files_cfg/hibernate-settings.xml" "$workDirectory/biserver-ce/pentaho-solutions/system/hibernate/"
+cp "$files_cfg/mysql5.hibernate.cfg.xml" "$workDirectory/biserver-ce/pentaho-solutions/system/hibernate/"
+cp "$files_cfg/quartz.properties" "$workDirectory/biserver-ce/pentaho-solutions/system/quartz/"
+cp "$files_cfg/repository.xml" "$workDirectory/biserver-ce/pentaho-solutions/system/jackrabbit/"
 
 echo "Copy success!"
