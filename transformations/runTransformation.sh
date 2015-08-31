f2fe49d82cbaf3e8736c4d8cd5172b74969c9242
pwener <phwener@gmail.com>
2015-08-31 16:06:57 -0300

    Refactor transformations and notify with comments next modifications

--- transformations/runTransformation.sh
+++ transformations/runTransformation.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-# load json file
+# Get an relative path of an log file
 json="$1"
-if [ -e $json ]; then
-	echo "Archive json found!"
-else
+
+# Verify if archive exist
+if [ ! -e $json ]; then
 	echo "Script fail because it can not find json file"
 	exit 0
 fi
 
+# Get absolut path of json file which 
+# has metrics about certain project
 path_json_file=$(cd $(dirname "$json") && pwd -P)/$(basename "$json")
 
-# verify if log file exist
+# Get an relative path of an log file
 log="$2"
-if [ -e $log ]; then
-	echo "Log file exist!"
-else
+
+# Verify if log file of kitchen exist
+if [ ! -e $log ]; then
 	touch "$log"
 	echo "Created..."
 fi
 
+# Get absolut path of $log file which has metrics about
+# about certain project
 path_log_file=$(cd $(dirname "$log") && pwd -P)/$(basename "$log")
 
+# Alterar para usar os diretorios onde for instalado o di
+pentahoDirectory="usr/esquadro/data-integration/kitchen.sh"
 
-pentahoDirectory="$HOME/App/data-integration/kitchen.sh"
-job="job.kjb"
-json="/home/pwener/Documentos/scripts/transformations/tests/104.json"
+#job="job.kjb"
 command="$pentahoDirectory -file=\"$job\" -level=RowLevel -param:JSON_FILE=\"$path_json_file\" -logfile=\"$path_log_file\""
+
 echo "$command"
 eval "$command"
