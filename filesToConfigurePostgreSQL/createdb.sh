2476729a2a44445be97ebd490c7c7e6008849a74
pwener <phwener@gmail.com>
2015-11-11 15:30:34 -0200

    Finishing the script to create database

--- filesToConfigurePostgreSQL/createdb.sh
+++ filesToConfigurePostgreSQL/createdb.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+#################################################
+#
+# This script needs be executed without user root,
+# because by default, postgresql have an role
+# named 'postgres' that have permission to create
+# and alter databases.
+#
+#################################################
+
+
+# Default user is postgres
 echo "Creating source info database..."
 
 POSTGRES_USER="postgres"
 
 createdb -O $POSTGRES_USER source_info
+echo "Success!"
 
-echo "Creating success"
+# Creating tables needed
+psql source_info -a -f createTables.sql
